5661e107e6dfa1b8fcc6570890350ab410cc4db7
Khanh Linh <khanhlinh.ph2002@gmail.com>
2023-05-21 15:03:50 +0700

    Test MongoDB Github Action

--- app/backend/tests/conftest.py
+++ app/backend/tests/conftest.py
@@ -2,9 +2,11 @@ import pytest
 import mongomock
 import sys
 
+CONNECTION_STRING = "mongodb://localhost:27017/"
+
 @pytest.fixture()
 def mongo_mock():
-    client = mongomock.MongoClient()
+    client = mongomock.MongoClient(CONNECTION_STRING)
     db = client.get_database("vdt-test")
     col = db.get_collection("attendees-test")
     emp_data = {
